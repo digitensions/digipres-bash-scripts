e1eb11621855bbd0707ad9b0415b08122f149d0e
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-04-23 17:27:11 +0100

    Update disk_space.sh

--- disk_space.sh
+++ disk_space.sh
@@ -6,6 +6,6 @@ df -H | grep '^storage_device_name' | awk '{ print $5 " " $6 " " $2 }' | while I
   partition=$(echo "$output" | awk '{ print $2 }' )
   max_size=$(echo "$output" | awk '{ print $3 }' )
   if [ "$used_space" -ge 85 ]; then
-      echo "Running out of space on qnap_h22, total space of $partition is $max_size, used space $used_space% as of $timestamp" | mail -s "Alert: Almost out of disk capacity $used_space% used" joanna.white@bfi.org.uk
+      echo "Running out of space on qnap_h22, total space of $partition is $max_size, used space $used_space% as of $timestamp" | mail -s "Alert: Almost out of disk capacity $used_space% used" email.to@bfi.org.uk
   fi
 done
