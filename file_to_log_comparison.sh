c1aac73aebcf0daa57747e85880cae8d843e8a3f
Joanna White <37188631+digitensions@users.noreply.github.com>
2020-07-02 14:52:05 +0100

    Addition of nested if statement
    This added to give additional detail for items that do no conform to simple True/False request, eg items partly through ingest but not yet deleted.

--- file_to_log_comparison.sh
+++ file_to_log_comparison.sh
@@ -16,12 +16,19 @@ echo "====== $timestamp ====== Comparing dpx_cooked folder to global.log ======"
 grep ^N ${log_path}temp_dpx_list.txt | while IFS= read -r files; do
  # Comparison of $files list against the global log, stored in variable $on_global
  on_global=$(grep "$files" "$global_log" | grep 'deleted')
- # if $on_global returns no string (ie, has no 'deleted' entry / any entry in global log)
- if [ -z "$on_global" ]
-   then
-     echo "****** ${files}.mkv has not passed into autoingest ******" >> ${log_path}global_log_check.log
-   # else, if it has an entry for both titled, and 'deleted'
-   else
-     echo "====== ${files}.mkv has been RAWcooked and is in Imagen ======" >> ${log_path}global_log_check.log
-  fi
+ # if $on_global returns a string it updates failure to log, if it has no 'deleted' entry in global log then passes to skipped variable.
+ # if $skipped returns no string then defaults to else statement. Otherwise updates log with file progress.
+ # Would case/esac work here instead of nested if statement? To be reviewed. Prob not necessary with just the one nest.
+  if [ -z "$on_global" ]
+        then
+            skipped=$(grep ${files} ${global_log} | grep 'Skip object')
+            if [ -z "$skipped" ]
+                then
+                  echo "****** ${files}.mkv HAS NOT PASSED INTO AUTOINGEST! ******" >> ${log_path}global_log_check.log
+                else
+                  echo "===== ${files}.mkv has been RAWcooked and is being ingested" >> ${log_path}global_log_check.log
+            fi
+        else
+            echo "====== ${files}.mkv has been RAWcooked and is in Imagen ======" >> ${log_path}global_log_check.log
+    fi
 done
